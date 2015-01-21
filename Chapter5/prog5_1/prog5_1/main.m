729d40bfe5b8c94dacd5eae98a9e2764c2fa92ee
Nicole Samson <nmsamson@outlook.com>
2015-01-21 12:42:35 -0600

    Housekeeping

---
+++ Chapter5/prog5_1/prog5_1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  prog5_1
+//
+//  Created by Nicole Samson on 1/21/15.
+//  Copyright (c) 2015 Nicole Samson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
