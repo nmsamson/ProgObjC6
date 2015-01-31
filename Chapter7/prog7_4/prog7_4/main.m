b84962428eea52789b979e8add3d106918833a12
Nicole Samson <nmsamson@outlook.com>
2015-01-30 20:17:20 -0600

    Chapter 7, Program 4
    Well, barely any of it.

---
+++ Chapter7/prog7_4/prog7_4/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  prog7_4
+//
+//  Created by Nicole Samson on 1/30/15.
+//  Copyright (c) 2015 Nicole Samson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
---
+++ Chapter7/prog7_4/prog7_4/prog7_4.m
@@ -0,0 +1,13 @@
+//
+//  prog7_4.m
+//  prog7_4
+//
+//  Created by Nicole Samson on 1/30/15.
+//  Copyright (c) 2015 Nicole Samson. All rights reserved.
+//
+
+#import "prog7_4.h"
+
+@implementation prog7_4
+
+@end
