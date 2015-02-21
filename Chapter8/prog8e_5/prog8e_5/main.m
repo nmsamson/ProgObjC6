eac06e46287b2880ba895ff0b578788122d750f2
Nicole Samson <nmsamson@outlook.com>
2015-02-20 18:12:40 -0600

    Chapter 8, Exercise 5

--- Chapter8/prog8e_5/prog8e_5/main.m
+++ Chapter8/prog8e_5/prog8e_5/main.m
@@ -6,13 +6,18 @@
 //  Copyright (c) 2015 Nicole Samson. All rights reserved.
 //
 
+#import "GraphicObject.h"
 #import "Rectangle.h"
+#import "Circle.h"
+#import "Triangle.h"
 #import "XYPoint.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         Rectangle *myRect = [[Rectangle alloc] init];
         XYPoint *myPoint = [[XYPoint alloc] init];
+        Circle *myCircle = [[Circle alloc] init];
+        Triangle *myTriangle = [[Triangle alloc] init];
         
         [myPoint setX: 100 andY: 200];
         
@@ -31,6 +36,16 @@ int main(int argc, const char * argv[]) {
         [myRect translate: movePoint];
         
         NSLog (@"Origin at (%g, %g)", myRect.origin.x, myRect.origin.y);
+        
+        myCircle.radius = 5;
+        NSLog (@"Circle radius = %g", myCircle.radius);
+        NSLog (@"Circle circumference = %g, area = %g", [myCircle circumference], [myCircle area]);
+        
+        [myTriangle setBase: 5 andHeight: 6];
+        NSLog (@"Triangle base = %g, height = %g", myTriangle.base, myTriangle.height);
+        NSLog (@"Triangle area = %g", [myTriangle area]);
+        [myTriangle setSide1: 5 andSide2: 6 andSide3: 7];
+        NSLog (@"Triangle side1 = %g, side2 = %g, side3 = %g, perimeter = %g", myTriangle.side1, myTriangle.side2, myTriangle.side3, [myTriangle perimeter]);
     }
     return 0;
 }
---
+++ Chapter8/prog8e_6/prog8e_6/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  prog8e_6
+//
+//  Created by Nicole Samson on 2/20/15.
+//  Copyright (c) 2015 Nicole Samson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
